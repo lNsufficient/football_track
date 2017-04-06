5a0c5f5800e10735bd02266729ec186f7ebaa857
Dabeza <dabeza@gmail.com>
2017-04-06 14:22:10 +0200

    Börjat med intressepunkter där bollen kan finnas
    pointsOfInterest tar 3 bildfiler (som borde vara i följd från videon)
    och ger tillbaka koordinater på ett antal punkter som är intressanta.

---
+++ pointsOfInterest.m
@@ -0,0 +1,12 @@
+function [ output_args ] = pointsOfInterest(frame_1,frame_2,frame_3)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    
+
+    diff_1 = abs(frame_1-frame2);
+    diff_2 = abs(frame_2-frame3);
+    
+    
+
+end
+
